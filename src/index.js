6840007bd74491012f6c3cb8e0c41b44a68b6bea
Muhammad Sabrino Raharjo <rinogawelz@gmail.com>
2023-01-12 11:59:06 +0700

    Add CRUD API notes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
 
 const init = async () => {
     const server = Hapi.server({
         port: 8005,
         host: 'localhost',
+        routes: {
+            cors: {
+                origin: ['*'],
+                headers: ["Accept", "Content-Type"],
+                additionalHeaders: ["X-Requested-With"]
+            },
+        },
     });
+
+    server.route(routes);
 
     await server.start();
     console.log(`Bookshelf API running at ${server.info.uri}`);
