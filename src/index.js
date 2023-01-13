5150f396da5ef9e0067a80b518ce28c615c768bf
Muhammad Sabrino Raharjo <rinogawelz@gmail.com>
2023-01-13 09:52:40 +0700

    Fixer code & add cors

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
-const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+const Hapi = require("@hapi/hapi");
+const routes = require("./routes");
 
 const init = async () => {
-    const server = Hapi.server({
-        port: 8005,
-        host: 'localhost',
-        routes: {
-            cors: {
-                origin: ['*'],
-                headers: ["Accept", "Content-Type"],
-                additionalHeaders: ["X-Requested-With"]
-            },
-        },
-    });
+  const server = Hapi.server({
+    port: 8005,
+    host: "0.0.0.0",
+    routes: {
+      cors: {
+        origin: ["*"],
+        headers: ["Accept", "Content-Type"],
+        exposedHeaders: ["Accept", "Content-Type"],
+        additionalHeaders: ["X-Requested-With"],
+      },
+    },
+  });
 
-    server.route(routes);
+  server.route(routes);
 
-    await server.start();
-    console.log(`Bookshelf API running at ${server.info.uri}`);
-}
+  await server.start();
+  console.log(`Bookshelf API running at ${server.info.uri}`);
+};
 
 init();
